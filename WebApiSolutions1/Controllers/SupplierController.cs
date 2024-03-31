4923d405a1988d52bb873d18e4e26a53f828abe4
Arif Ozan AKTAŞ <aoaktas@hotmail.com>
2024-03-31 11:37:35 +0300

    HttpDelete and HttpPut added

--- WebApiSolutions1/Controllers/SupplierController.cs
+++ WebApiSolutions1/Controllers/SupplierController.cs
@@ -25,4 +25,34 @@ public class SupplierController : ControllerBase
         }
         return Ok(supplier);
     }
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+        NorthwndContext context = new();
+        Supplier supplier = context.Suppliers.FirstOrDefault(y=>y.SupplierId == id);
+        if(supplier == null)
+        {
+            return BadRequest("Böyle bir Supplier bulunamadı!");
+        }
+        context.Suppliers.Remove(supplier);
+        context.SaveChanges();
+        return Ok("Kayıt başarılı");
+    }
+    [HttpPut]
+    public IActionResult Put(Supplier supplier)
+    {
+        NorthwndContext context= new NorthwndContext();
+        Supplier supplier1 = context.Suppliers.FirstOrDefault(x=>x.SupplierId==supplier.SupplierId);
+        if(supplier1 == null)
+        {
+            return BadRequest("Böyle bir Supplier bulunamadı !");
+        }
+        supplier1.ContactName = supplier.ContactName;
+        supplier1.CompanyName = supplier.CompanyName;
+        supplier1.Address = supplier.Address;
+        supplier1.ContactTitle = supplier.ContactTitle;
+        supplier1.Phone = supplier.Phone;
+        context.SaveChanges();
+        return Ok("Kayıt başarılı");
+    }
 }
